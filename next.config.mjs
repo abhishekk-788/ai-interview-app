2faeafb1730c69c7d03faaf6fcc57b8e30c00626
abhishekk-788 <abhishekkumar@Abhisheks-MacBook-Air.local>
2024-11-15 08:33:15 +0530

    Redirect to Dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true, // Set to true for a permanent redirect (308 status)
+      },
+    ];
+  },
+};
 
 export default nextConfig;
